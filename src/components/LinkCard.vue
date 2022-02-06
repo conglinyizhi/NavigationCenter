4920e7063569e843d35d258b52aaa7b66cae8aaf
丛林意志 <2523090520@qq.com>
2022-02-06 19:45:40 +0800

    add LinkCard

---
+++ src/components/LinkCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="linkCard">
+    <div
+      class="title"
+      v-text="title"
+    ></div>
+    <div><a :href="link"></div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  link: String,
+});
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+.linkCard {
+  border: 1px #42b983 groove;
+  padding: 3rem;
+  margin: 3rem;
+}
+
+.linkCard .title {
+  margin-bottom: 1.5rem;
+}
+</style>
