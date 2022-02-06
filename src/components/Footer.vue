7860aa83fdebea445e0f5847b333f12043022a71
丛林意志 <2523090520@qq.com>
2022-02-06 21:52:51 +0800

    Change Footer - PowerBy

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-    <div id="footer">Powery By <a href="https://vercel.com/">Vercel</a></div>
+  <div id="footer">Powery By <a href="https://vercel.com/">Vercel</a> &amp; <a
+      href="https://v3.vuejs.org/"
+      target="_blank"
+    >Vue 3</a>
+  </div>
 </template>
 
 <script setup>
@@ -7,7 +11,7 @@ defineProps({});
 </script>
 
 <style scoped>
-#footer{
-    text-align: center;
+#footer {
+  text-align: center;
 }
 </style>
