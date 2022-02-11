d3db29efcfe9306ed72e956935015e9e553ce778
丛林意志 <2523090520@qq.com>
2022-02-11 18:02:45 +0800

    初步完成搜索引擎区块搭建

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <Header/>
+  <Search/>
   <Navigation/>
   <Footer/>
 </template>
@@ -8,6 +9,7 @@
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 import Navigation from './components/Navigation.vue'
+import Search from './components/Search.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
---
+++ src/components/Search.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="search">
+    <span @click="changeWebsite">切换引擎：Bing</span>
+    <input type="text" name="keywords" id="keywordsInput" v-model="keywords" @keydown.enter="search" />
+    <span @click="search">搜索</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  props: {
+    //为组件注册属性
+    searchWebsite: {
+      //规范写法
+      type: String,
+      default: "bing",
+    },
+  },
+  methods: {
+    search(){
+      console.log(this.keywords)
+    },changeWebsite(){
+      console.log("更换搜索引擎")
+    }
+  },
+  data: function () {
+    //为组件注册数据
+    return {
+      keywords:""
+    };
+  },
+};
+// defineProps({keywords:""});
+</script>
+
+<style scoped>
+#search {
+  display: block;
+  margin: 3px;
+  border-radius: 1.3rem;
+  padding: 3px 0;
+  height: 1.3rem;
+  border: 1px #ccc groove;
+}
+
+/* #search::before {
+  content: "";
+  width: 0;
+  height: 0;
+  border: 20px solid;
+  position: absolute;
+  top: 0px;
+  left: 140px;
+  border-color: #ff0 transparent transparent;
+} */
+
+input {
+  border: 0;
+}
+</style>
