5362beb5527a7174b8ebff837ed7e61d8278ed73
丛林意志 <2523090520@qq.com>
2022-02-11 14:38:19 +0800

    修改结构

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,13 @@
 <template>
   <Header/>
-  <LinkCard title="萌导航 - 二次元导航中心" link="https://www.moe123.net"/>
-  <LinkCardGroup title="开发者中心">
-    <LinkCard title="OpenCV C++ 文档" link="https://docs.opencv.org/3.4.0"/>
-    <LinkCard title="Github 开源社区" link="https://www.github.com"/>
-    <LinkCard title="Gitee 开源" link="https://www.github.com"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="游戏官网">
-    <LinkCard title="Genshin" link="https://ys.mihoyo.com/main/"/>
-    <LinkCard title="米哈游" link="https://mihoyo.com"/>
-    <LinkCard title="腾讯游戏" link="https://game.qq.com"/>
-    <LinkCard title="完美世界" link="https://game.wanmei.com"/>
-    <LinkCard title="网易游戏" link="https://game.163.com"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="信息获取平台">
-    <LinkCard title="AcFun" link="https://www.acfun.com"/>
-    <LinkCard title="Bilibili" link="https://www.bilibili.com"/>
-    <LinkCard title="萌娘百科" link="https://zh.moegirl.org.cn"/>
-    <LinkCard title="小鸡词典" link="https://jikipedia.com"/>
-    <LinkCard title="知乎日报" link="https://daily.zhihu.com/"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="工具类站点">
-    <LinkCard title="QQ 邮箱" link="https://mail.qq.com"/>
-    <LinkCard title="网易邮箱" link="https://mail.163.com"/>
-    <LinkCard title="百度网盘" link="https://pan.baidu.com"/>
-    <LinkCard title="网易音乐" link="https://music.163.com/"/>
-    <LinkCard title="腾讯音乐" link="https://music.qq.com/"/>
-    <LinkCard title="喜马拉雅" link="https://www.ximalaya.com"/>
-  </LinkCardGroup>
+  <Navigation/>
   <Footer/>
 </template>
 
 <script setup>
-import LinkCard from './components/LinkCard.vue'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
-import LinkCardGroup from './components/LinkCardGroup.vue'
+import Navigation from './components/Navigation.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
