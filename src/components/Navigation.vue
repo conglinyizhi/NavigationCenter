e2d74b35f4fd47aefbf8fb6596d5e6ee0fe46393
丛林意志 <2523090520@qq.com>
2022-02-12 16:39:21 +0800

    通过 v-for 降低代码量

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,61 +1,123 @@
 <template>
-  <LinkCardGroup title="其他的导航网站">
-    <LinkCard title="萌导航" link="https://www.moe123.net"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="邮箱">
-    <LinkCard title="QQ 邮箱" link="https://mail.qq.com"/>
-    <LinkCard title="网易邮箱" link="https://mail.163.com"/>
-    <LinkCard title="GMail" link="https://pan.baidu.com"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="文档">
-    <LinkCard title="OpenCV" link="https://docs.opencv.org/3.4.0"/>
-    <LinkCard title="Vue.js" link="https://cn.vuejs.org"/>
-    <LinkCard title="MDUI.js" link="https://www.mdui.org/docs/"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="开发者之家">
-    <LinkCard title="Github" link="https://www.github.com"/>
-    <LinkCard title="Gitee" link="https://www.github.com"/>
-    <LinkCard title="思否" link="https://segmentfault.com"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="云服务提供者">
-    <LinkCard title="阿里云" link="https://cloud.aliyun.com"/>
-    <LinkCard title="腾讯云" link="https://cloud.tencent.com"/>
-    <LinkCard title="百度云" link="https://cloud.baidu.com"/>
-    <LinkCard title="华为云" link="https://cloud.huawei.com"/>
-    <LinkCard title="字节云" link="https://qingfuwu.cn"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="游戏官网">
-    <LinkCard title="Genshin" link="https://ys.mihoyo.com/main/"/>
-    <LinkCard title="米哈游" link="https://mihoyo.com"/>
-    <LinkCard title="腾讯游戏" link="https://game.qq.com"/>
-    <LinkCard title="完美世界" link="https://game.wanmei.com"/>
-    <LinkCard title="网易游戏" link="https://game.163.com"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="视频网站">
-    <LinkCard title="AcFun" link="https://www.acfun.com"/>
-    <LinkCard title="Bilibili" link="https://www.bilibili.com"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="音乐网站与有声读物">
-    <LinkCard title="网易音乐" link="https://music.163.com/"/>
-    <LinkCard title="腾讯音乐" link="https://music.qq.com/"/>
-    <LinkCard title="喜马拉雅" link="https://www.ximalaya.com"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="信息获取平台">
-    <LinkCard title="萌娘百科" link="https://zh.moegirl.org.cn"/>
-    <LinkCard title="小鸡词典" link="https://jikipedia.com"/>
-    <LinkCard title="知乎日报" link="https://daily.zhihu.com/"/>
-  </LinkCardGroup>
-  <LinkCardGroup title="没分类">
-    <LinkCard title="百度网盘" link="https://pan.baidu.com"/>
+  <LinkCardGroup
+    v-for="(a,i) in siteData"
+    :key="i"
+    :title="a.title"
+  >
+    <LinkCard
+      v-for="(site,key) in a.list"
+      :key="key"
+      :title="site.title"
+      :link="site.url"
+    />
   </LinkCardGroup>
 </template>
 
-<script setup>
-import LinkCard from './LinkCard.vue'
-import LinkCardGroup from './LinkCardGroup.vue'
-defineProps({});
+<script>
+import LinkCard from "./LinkCard.vue";
+import LinkCardGroup from "./LinkCardGroup.vue";
+const siteData = [
+  {
+    title: "其他的导航网站",
+    list: [
+      { title: "萌导航", url: "https://www.moe123.net" },
+      { title: "个站商店", url: "https://storeweb.cn" },
+    ],
+  },
+  {
+    title: "邮箱",
+    list: [
+      { title: "QQ 邮箱", url: "https://mail.qq.com" },
+      { title: "网易邮箱", url: "https://email.163.com" },
+      { title: "GMail", url: "http://mail.google.com/mail?hl=zh-CN" },
+      { title: "新浪邮箱", url: "https://mail.sina.com.cn" },
+      { title: "Outlook", url: "https://outlook.live.com/mail/0/" },
+    ],
+  },
+  {
+    title: "文档",
+    list: [
+      { title: "OpenCV", url: "https://docs.opencv.org/3.4.0" },
+      { title: "Vue.js", url: "https://cn.vuejs.org" },
+      { title: "MDUI.js", url: "https://www.mdui.org/docs/" },
+    ],
+  },
+  {
+    title: "开发者之家",
+    list: [
+      { title: "Github", url: "https://www.github.com" },
+      { title: "Gitee", url: "https://www.github.com" },
+      { title: "思否", url: "https://segmentfault.com" },
+    ],
+  },
+  {
+    title: "云服务提供者",
+    list: [
+      { title: "阿里云", url: "https://cloud.aliyun.com" },
+      { title: "腾讯云", url: "https://cloud.tencent.com" },
+      { title: "百度云", url: "https://cloud.baidu.com" },
+      { title: "华为云", url: "https://cloud.huawei.com" },
+      { title: "字节云", url: "https://qingfuwu.cn" },
+    ],
+  },
+  {
+    title: "游戏官网",
+    list: [
+      { title: "Genshin", url: "https://ys.mihoyo.com/main/" },
+      { title: "米哈游", url: "https://mihoyo.com" },
+      { title: "腾讯游戏", url: "https://game.qq.com" },
+      { title: "完美世界", url: "https://game.wanmei.com" },
+      { title: "网易游戏", url: "https://game.163.com" },
+    ],
+  },
+  {
+    title: "视频网站",
+    list: [
+      { title: "AcFun", url: "https://www.acfun.com" },
+      { title: "Bilibili", url: "https://www.bilibili.com" },
+    ],
+  },
+  {
+    title: "音乐网站与有声读物",
+    list: [
+      { title: "网易音乐", url: "https://music.163.com/" },
+      { title: "腾讯音乐", url: "https://music.qq.com/" },
+      { title: "喜马拉雅", url: "https://www.ximalaya.com" },
+    ],
+  },
+  {
+    title: "现代互联网冲浪使用说明",
+    list: [
+      { title: "萌娘百科", url: "https://zh.moegirl.org.cn" },
+      { title: "小鸡词典", url: "https://jikipedia.com" },
+      { title: "知乎日报", url: "https://daily.zhihu.com/" },
+    ],
+  },
+  {
+    title: "现代互联网冲浪使用说明",
+    list: [
+      { title: "萌娘百科", url: "https://zh.moegirl.org.cn" },
+      { title: "小鸡词典", url: "https://jikipedia.com" },
+      { title: "知乎日报", url: "https://daily.zhihu.com/" },
+    ],
+  },
+  {
+    title: "没分类",
+    list: [{ title: "百度网盘", url: "https://pan.baidu.com" }],
+  },
+];
+export default {
+  data() {
+    return {
+      siteData,
+    };
+  },
+  components: {
+    LinkCard,
+    LinkCardGroup,
+  },
+};
 </script>
 
 <style>
-
 </style>
