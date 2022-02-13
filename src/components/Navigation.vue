a3d4c736718087696ff277231ed0233292fb9c25
丛林意志 <2523090520@qq.com>
2022-02-13 15:41:52 +0800

    修复 因为删除组件导致的样式丢失问题

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -156,4 +156,51 @@ export default {
 </script>
 
 <style>
+.linkCardGroup {
+  border: 1px #42b98377 groove;
+  padding: 3px;
+  margin: 3px;
+  text-align: left;
+  border-radius: 6px;
+  margin: 3px 5px;
+  padding-left: 4px;
+}
+@media screen and (min-width: 900px) {
+  .linkCardGroup {
+    width: 80%;
+    display: inline-block;
+  }
+}
+.title {
+  padding: 0 5px 3px;
+  display: flex;
+}
+.title span {
+  flex: 1;
+}
+.title .right {
+  text-align: right;
+}
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+.linkCard {
+  border: 1px #42b9832f groove;
+  padding: 3px;
+  margin: 3px;
+  text-align: center;
+  border-radius: 3px;
+  display: inline-block;
+  width: 10%;
+  min-width: 4rem;
+  max-width: 7rem;
+  line-height: 1.1rem;
+}
+.linkCard:hover {
+  border-color: #42b983;
+}
+.linkCard .title {
+  margin-bottom: 0.5rem;
+}
 </style>
