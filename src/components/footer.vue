c61e36ac32929f712604a8de22e14560c0128565
丛林意志 <2523090520@qq.com>
2022-02-15 22:59:25 +0800

    新增 钟离台词

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="footer">
-    <div>Powery By <a href="https://vercel.com/">Vercel</a> &amp; <a
-      href="https://v3.vuejs.org/"
-      target="_blank"
-    >Vue 3</a></div>
+    <div @click="showDebugData">
+      我虽无意逐鹿，却知苍生苦楚 ——原神 钟离
+    </div>
+    <div>Powery By <a
+        href="https://v3.vuejs.org/"
+        target="_blank"
+      >Vue 3</a> | 托管在 <a href="https://vercel.com/">Vercel</a></div>
     <div>如果需要提交网站…… 目前没有提交渠道</div>
   </div>
 </template>
 
-<script setup>
-defineProps({});
+<script>
+export default {
+  name: "footer",
+  data() {
+    return {
+      debugdata: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+      methods: {
+        showDebugData() {
+          alert(
+            `此世诸神群魔并起，我虽无意逐鹿，却知苍生苦楚。\n屏幕尺寸：${this.width}x${this.height}`
+          );
+        },
+      },
+    };
+  },
+};
 </script>
 
 <style scoped>
 #footer {
   text-align: center;
+  padding-bottom: 1rem;
+}
+
+#footer > div {
+  margin: 10px;
+}
+
+#debugtip {
+  border: 1px #ccc groove;
+  display: inline-block;
+  padding: 0 10px;
+  border-radius: 10px;
 }
 </style>
