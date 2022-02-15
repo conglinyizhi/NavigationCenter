1ba3e6ad01de0c026b676e46f01848b06ff88421
丛林意志 <2523090520@qq.com>
2022-02-15 23:35:37 +0800

    增加 动态效果

--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>进度条</h1>
+    <div title="鼠标放上去或者点一下">充满希望的进度条，或许某个时候他会跑满</div>
     <div class="programBarFather">
       <div class="programBar"></div>
     </div>
@@ -9,15 +10,19 @@
 
 <style>
 .programBar {
-  width: 23%;
-  height: 10px;
+  width: 61%;
+  height: 20px;
   background: #6cf;
-  border-radius: 10px;
+  border-radius: 20px;
+  transition: width 1s;
+}
+.programBar:hover,.programBarFather:hover div {
+  width: 100%;
 }
 
 .programBarFather {
   background: #666;
   border-radius: 10px;
-  margin:10px;
+  margin: 10px;
 }
 </style>
