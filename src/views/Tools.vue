087d47daddfa1dfc5676aaac5fdb3e6e8b9e73c7
丛林意志 <2523090520@qq.com>
2022-02-15 17:29:55 +0800

    新增 工具页面放个进度条

--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>进度条</h1>
+    <div class="programBarFather">
+      <div class="programBar"></div>
+    </div>
   </div>
 </template>
+
+<style>
+.programBar {
+  width: 23%;
+  height: 10px;
+  background: #6cf;
+  border-radius: 10px;
+}
+
+.programBarFather {
+  background: #666;
+  border-radius: 10px;
+  margin:10px;
+}
+</style>
