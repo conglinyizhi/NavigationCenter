6875e6a2fc37ba795443ded7b3809adfebfdac35
丛林意志 <2523090520@qq.com>
2022-02-17 01:13:20 +0800

    尝试修复编译错误

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig, ProvidePlugin } from 'vite'
+import { defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import bootstrap from 'bootstrap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-  new ProvidePlugin({
-    $: "jquery",
-    jquery: "jquery",
-    jQuery: "jquery",
-    "window.jQuery": "jquery",
-  })]
+  plugins: [vue()]
 })
