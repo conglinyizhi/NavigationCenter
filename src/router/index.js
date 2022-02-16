489d1934a116704224bd561d325ee4dfa65d8e17
丛林意志 <2523090520@qq.com>
2022-02-16 21:20:40 +0800

    修改 tools.vue 为 tools_interface.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Tools from '../views/Tools.vue'
-import Debug from '../views/debug.vue'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -13,18 +11,15 @@ const router = createRouter({
     {
       path: '/about',
       name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    },{
-      path:'/tools',
-      name:'Tools',
-      component:Tools
-    },{
-      path:'/debug',
-      name:'debug',
-      component:Debug
+      component: () => import('../views/About.vue')
+    }, {
+      path: '/tools',
+      name: 'Tools',
+      component: () => import('../views/tools_interface.vue')
+    }, {
+      path: '/debug',
+      name: 'debug',
+      component: () => import('../views/debug.vue')
     }
   ]
 })
